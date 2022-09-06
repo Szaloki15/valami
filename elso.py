1c7165da5c621a938a639f494183fa6681cfd621
Szalóki Márton <Szalokim@KKSZKI.LOCAL>
2022-09-06 14:36:16 +0200

    v1.0.2

--- elso.py
+++ elso.py
@@ -1,6 +1,1 @@
-print("Go, go, go, go, go, go\
-Go, shorty\
-It's your birthday\
-We gon' party like it's your birthday\
-We gon' sip Bacardi like it's your birthday\
-And you know we don't give a fuck it's not your birthday")
+print("elso sor\n masodik sor\n harmadik sor \n\")
