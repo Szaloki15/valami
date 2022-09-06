30817f98de39295786329e1704ce1e21e9fd940b
Szalóki Márton <Szalokim@KKSZKI.LOCAL>
2022-09-06 14:32:44 +0200

    v1.0.1

---
+++ elso.py
@@ -0,0 +1,6 @@
+print("Go, go, go, go, go, go\
+Go, shorty\
+It's your birthday\
+We gon' party like it's your birthday\
+We gon' sip Bacardi like it's your birthday\
+And you know we don't give a fuck it's not your birthday")
